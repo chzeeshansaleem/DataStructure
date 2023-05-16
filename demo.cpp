b366ef831c3f04f1768e7d6161239af121f0651b
ch zeeshan <chzeeshan1322@gmail.com>
2023-05-16 14:26:42 +0500

    interview

--- demo.cpp
+++ demo.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main(){
     cout << "Helo world" << endl;
     cout << "MERN Stack Devloperator"<<endl;
+    cout << "interview ki tyari kr ly kaka"<<endl;
     return 0;
 }
